e4a9e037e0daf21fd4f1232d22f55725b481f83a
Nick Junius <njunius@users.noreply.github.com>
2018-04-25 22:20:04 -0700

    CH Torso Rotation Update
    CH Torso now rotates at a constant rate towards the mouse using the rigidbody attached to the parent object

--- Mech_Sim_Prototype/Assets/Scripts/CH Mech Systems/CH_Mech_PlayerController.cs
+++ Mech_Sim_Prototype/Assets/Scripts/CH Mech Systems/CH_Mech_PlayerController.cs
@@ -7,28 +7,26 @@ public class CH_Mech_PlayerController : MonoBehaviour {
     public Transform torsoTransform;
     private Quaternion torsoRotation;
     private Vector3 mouseLoc;
-    private Vector3 prevMouseLoc;
     private Vector3 directionToRotateToward;
-    private Quaternion mouseRotationAngle;
 
     private float forwardDirectionOffset;
+    public float degreesToMouse;
 
-    // in degrees
+    // rotation speed in degrees
     private float torsoRotSpeed;
     private float legsRotSpeed;
 
-    public float degreesToMouse;
-    
+    // torso rotation book-keeping
+    private Quaternion degreesToMouseToQuaternion;
+    private float rotationIncrement;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody2D>();
-        torsoTransform = GetComponent<Transform>();
         torsoRotation = torsoTransform.rotation;
         mouseLoc = Input.mousePosition;
-        prevMouseLoc = mouseLoc;
         forwardDirectionOffset = 90.0f;
-        torsoRotSpeed = 1.5f;
+        torsoRotSpeed = 2.0f;
 	}
 	
 	// Update is called once per frame
@@ -41,13 +39,11 @@ public class CH_Mech_PlayerController : MonoBehaviour {
     void FixedUpdate() {
         // torso rotation code
         mouseLoc = Input.mousePosition;
-        // only update degreesToMouse when necessary
-        if (prevMouseLoc != mouseLoc) {
-            torsoRotation = torsoTransform.rotation;
-            directionToRotateToward = Camera.main.ScreenToWorldPoint(mouseLoc) - torsoTransform.position;
-            degreesToMouse = Mathf.Atan2(directionToRotateToward.y, directionToRotateToward.x) * Mathf.Rad2Deg - forwardDirectionOffset;
-        }
-        rigidBody.MoveRotation(Mathf.LerpAngle(rigidBody.rotation, degreesToMouse, torsoRotSpeed * Time.deltaTime));
-        prevMouseLoc = mouseLoc;
+        torsoRotation = torsoTransform.rotation;
+        directionToRotateToward = Camera.main.ScreenToWorldPoint(mouseLoc) - torsoTransform.position;
+        degreesToMouse = Mathf.Atan2(directionToRotateToward.y, directionToRotateToward.x) * Mathf.Rad2Deg - forwardDirectionOffset;
+        degreesToMouseToQuaternion = Quaternion.Euler(0f, 0f, degreesToMouse);
+        rotationIncrement = Quaternion.RotateTowards(torsoRotation, degreesToMouseToQuaternion, torsoRotSpeed).eulerAngles.z;
+        rigidBody.MoveRotation(rotationIncrement);
     }
 }
