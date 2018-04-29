75855a4d1e469339d3a74d8876bec6455f733eff
Nick Junius <njunius@users.noreply.github.com>
2018-04-29 15:18:32 -0700

    Added Acceleration
    CH Mechs now have acceleration when starting and stopping movement
    
    Still need to make transition from moving 1 direction to its opposite accelerate

--- Mech_Sim_Prototype/Assets/Scripts/CH Mech Systems/CH_Mech_PlayerController.cs
+++ Mech_Sim_Prototype/Assets/Scripts/CH Mech Systems/CH_Mech_PlayerController.cs
@@ -19,7 +19,7 @@ public class CH_Mech_PlayerController : MonoBehaviour {
     private Rigidbody2D legsRigidBody;
     private float roundedLegsRot;
     private Vector3 legsXYRot;
-
+    private Vector3 checkBackwards;
     private RotateTowardMouse rotationFunction;
     private float angleBetweenTorsoAndLegs;
 
@@ -28,8 +28,10 @@ public class CH_Mech_PlayerController : MonoBehaviour {
     private float legsRotSpeed;
 
     private float maxMoveSpeed;
+    private float maxRevSpeed;
     private float currMoveSpeed;
     private float accelerationRate;
+    private bool moving;
 
     // rotation book-keeping
     private float torsoRotationIncrement;
@@ -54,8 +56,10 @@ public class CH_Mech_PlayerController : MonoBehaviour {
         torsoRotSpeed = 2.0f;
         legsRotSpeed = 1.5f;
         maxMoveSpeed = 0.7f;
+        maxRevSpeed = -0.7f;
         currMoveSpeed = 0.0f;
-        accelerationRate = 0.05f;
+        accelerationRate = 0.004f;
+        moving = false;
 	}
 	
 	// Update is called once per frame
@@ -71,51 +75,101 @@ public class CH_Mech_PlayerController : MonoBehaviour {
 
         angleBetweenTorsoAndLegs = Mathf.Abs(Mathf.DeltaAngle(roundedTorsoRot, roundedLegsRot));
 
+        Debug.Log(torsoRigidBody.velocity);
+
         if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) {
-            legsXYRot = Vector3.zero;
-        }
+            //legsXYRot = Vector2.zero;
+
+            if (currMoveSpeed > 0) {
+                currMoveSpeed -= accelerationRate;
+                currMoveSpeed = Mathf.Max(0f, currMoveSpeed);
+            }
+            //else {
+            //    currMoveSpeed += accelerationRate;
+            //    currMoveSpeed = Mathf.Min(0f, currMoveSpeed);
+            //}
             
+        }
+
         if (Input.GetKey(KeyCode.W)) { // go forward relative to the direction the torso is facing
             if (angleBetweenTorsoAndLegs == 180 || angleBetweenTorsoAndLegs == 0) {
                 legsXYRot += legsTransform.right;
+                currMoveSpeed += accelerationRate;
             }
             else {
                 handleLegRotation(torsoTransform.rotation, legsTransform.right);
             }
         }
-        if (Input.GetKey(KeyCode.S)) { // go backward relative to the direction the torso is facing
+        if (Input.GetKey(KeyCode.S)) { // go backward relative to the direction the legs are facing
             if (angleBetweenTorsoAndLegs == 180 || angleBetweenTorsoAndLegs == 0) {
                 legsXYRot -= legsTransform.right;
+                //moving = true;
+                //if (currMoveSpeed > 0 && torsoRigidBody.velocity != Vector2.zero)
+                //    currMoveSpeed -= accelerationRate;
+                //else
+                currMoveSpeed += accelerationRate;
             }
             else {
                 handleLegRotation(torsoTransform.rotation, -legsTransform.right);
             }
         }
+        legsXYRot.Normalize();
         if (Input.GetKey(KeyCode.A)) { // go left relative to the direction the torso is facing
             if (angleBetweenTorsoAndLegs == 90) {
-                legsXYRot += legsTransform.right;
+                if (Input.GetKey(KeyCode.S)) {
+                    legsXYRot -= legsTransform.right;
+                    //currMoveSpeed -= accelerationRate;
+                }
+                else {
+                    legsXYRot += legsTransform.right;
+                    currMoveSpeed += accelerationRate;
+                }
+                //legsXYRot += legsTransform.right;
             }
             else {
                 torsoRotPerpToMove += 90f;
-                handleLegRotation(Quaternion.Euler(torsoTransform.rotation.eulerAngles.x, torsoTransform.rotation.eulerAngles.y, torsoRotPerpToMove), legsTransform.right);
+                checkBackwards = legsTransform.right;
+                if (Input.GetKey(KeyCode.S)) {
+                    checkBackwards = -legsTransform.right;
+                }
+                handleLegRotation(Quaternion.Euler(torsoTransform.rotation.eulerAngles.x, torsoTransform.rotation.eulerAngles.y, torsoRotPerpToMove), checkBackwards);
             }
         }
         if (Input.GetKey(KeyCode.D)) { // go right relative to the direction the torso is facing
             if (angleBetweenTorsoAndLegs == 90) {
-                legsXYRot += legsTransform.right;
+                if (Input.GetKey(KeyCode.S)) {
+                    legsXYRot -= legsTransform.right;
+                    //currMoveSpeed -= accelerationRate;
+                }
+                else {
+                    legsXYRot += legsTransform.right;
+                    currMoveSpeed += accelerationRate;
+                }
+                //legsXYRot += legsTransform.right;
             }
             else {
                 torsoRotPerpToMove -= 90f;
-                handleLegRotation(Quaternion.Euler(torsoTransform.rotation.eulerAngles.x, torsoTransform.rotation.eulerAngles.y, torsoRotPerpToMove), legsTransform.right);
+                checkBackwards = legsTransform.right;
+                if (Input.GetKey(KeyCode.S)) {
+                    checkBackwards = -legsTransform.right;
+                }
+                handleLegRotation(Quaternion.Euler(torsoTransform.rotation.eulerAngles.x, torsoTransform.rotation.eulerAngles.y, torsoRotPerpToMove), checkBackwards);
             }
         }
         legsXYRot.Normalize();
 
+        //if (currMoveSpeed < 0) {
+            //currMoveSpeed = Mathf.Max(maxRevSpeed, currMoveSpeed);
+        //}
+        //else {
+            currMoveSpeed = Mathf.Min(maxMoveSpeed, currMoveSpeed);
+        //}
     }
 
     void FixedUpdate() {
         // movement code
-        torsoRigidBody.velocity = legsXYRot * maxMoveSpeed;
+        Debug.Log("currMoveSpeed: " + currMoveSpeed);
+        torsoRigidBody.velocity = legsXYRot * currMoveSpeed;
 
         // torso rotation code
         if (legsXYRot == Vector3.zero) {
