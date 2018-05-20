0580aebb7a6265e53d8f730714c37c2c2cc76b64
Nick Junius <njunius@users.noreply.github.com>
2018-05-20 15:04:14 -0700

    Started UI Mockup
    Added placeholder elements

---
+++ Mech_Sim_Prototype/Assets/Scripts/UI/WeaponGroupController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponGroupController : MonoBehaviour {
+    public int numGroups;
+    public GameObject groupDisplay;
+    private Image[] groupTracker;
+
+
+
+	// Use this for initialization
+	void Start () {
+        groupTracker = new Image[numGroups];
+        
+        for (int i = 0; i < numGroups; ++i) {
+            GameObject temp = (GameObject)Instantiate(groupDisplay, gameObject.transform.position, gameObject.transform.rotation);
+            temp.transform.SetParent(gameObject.transform, false);
+            groupTracker[i] = temp.GetComponent<Image>();
+            if(i % 2 != 0) {
+                groupTracker[i].gameObject.transform.Rotate(new Vector3(180, 0, 0));
+            }
+        }	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
